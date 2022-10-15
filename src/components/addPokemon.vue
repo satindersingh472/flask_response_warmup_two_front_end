c5aa81c9fa49a458deabd2b62041b5f18c5fa6db
satinder <satindersingh472@gmail.com>
2022-10-15 13:00:24 -0600

    column_layout

--- src/components/addPokemon.vue
+++ src/components/addPokemon.vue
@@ -46,4 +46,10 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.all_inputs{
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+}
+
+</style>
